cc20384c13f4305f0a23982ccb9c9b3a670c458c
Anderson Rodrigues <maikbru@gmail.com>
2021-04-28 12:26:34 -0300

    Initial commit

---
+++ src/main/java/br/com/desenv/monitor/MonitorApplication.java
@@ -0,0 +1,17 @@
+package br.com.desenv.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/desenv/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desenv.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
